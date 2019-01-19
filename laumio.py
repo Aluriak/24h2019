f34c48422cf09de5a96655fc00ff784c416fb0e0
Jnsll <june.benvegnu-sallou@irisa.fr>
2019-01-19 14:40:14 +0100

    definition of functions in the Laumio class

--- laumio.py
+++ laumio.py
@@ -1,11 +1,35 @@
 #/usr/bin/env python3
 
-import socket
-import urllib.request
-
 class Laumio:
     """ 
     """
-    status = ''
-    name = ''
 
+    def off():
+        """ """
+        ...
+
+    # function changing the color of the whole laumio
+    def all_blue():
+        ...
+
+    def all_color():
+        ...
+
+
+    # functions dealing with the colors of the rings of the laumio 
+    def bottom_ring():
+        """ """
+        ...
+
+    def middle_ring():
+        """ """
+        ...
+    
+    def top_ring():
+        """ """
+        ...
+
+
+
+    def __init__(self, name):
+        self.name = name 
