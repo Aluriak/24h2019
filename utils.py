e206c9a90e89d94fd392db6af5ce8095bcba1638
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2019-01-19 15:04:46 +0100

    color converter in utils

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 
 import sys
 import uuid
+from matplotlib import colors
 import traceback
 from functools import wraps
 
@@ -27,3 +28,17 @@ def create_client(servername:str='localhost', port:int=1883, id_prefix:str='TBC_
     client = mqtt.Client(client_id=id_prefix + str(uuid.uuid4()))
     client.connect(servername, port=port)
     return client
+
+
+def colorname_to_rgb(name:str) -> (int, int, int):
+    """
+
+    >>> colorname_to_rgb('red')
+    (255, 0, 0)
+    >>> colorname_to_rgb('blue')
+    (0, 0, 255)
+
+    """
+    float_to_int = lambda x: int(round(x*255, 0))
+    return tuple(map(float_to_int, colors.to_rgba(name)[:3]))
+
