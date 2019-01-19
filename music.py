a26a452bb0f5453837d102e761f391ffd6209f92
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2019-01-19 19:59:42 +0100

    bugfixes

--- music.py
+++ music.py
@@ -1,16 +1,22 @@
+"""À écrire"""
+
 import utils
 import conf
+
 
 def music_control(client, cmd, value=None):
     topic = conf.COMMAND_MUSIC.format(cmd=cmd)
     if cmd == 'setvol':
         utils.send_though_client(client, topic, value)
     elif cmd == 'getstate':
+        status = None
         def on_state_music(client, userdata, message):
+            nonlocal status
             status = message.payload.decode()
         client.on_message = on_state_music
         client.subscribe('music/status')
         utils.send_through_client(client, topic)
+        time.sleep(1)
         return status
     elif cmd == 'getvol':
         def on_volume_music(client, userdata, message):
