183b8e316fb5ebbbf1753758aa463d9ad2d51267
Jnsll <june.benvegnu-sallou@irisa.fr>
2019-01-19 15:16:50 +0100

    update function changing the color of the whole laumio

--- laumio.py
+++ laumio.py
@@ -23,12 +23,13 @@ class Laumio:
         self.client.publish(topic, payload=blue)
 
     def all_color([r, g, b]):
-
         ...
 
     def all_color(color):
-
-        ...
+        """ Change the color of all the LEDs of the laumio to chosen color"""
+        rgb = utils.rgb_from_colorname(color)
+        topic = COMMAND_TARGET_TOPIC.format(name=self.name, cmd="fill")
+        self.client.publish(topic, payload=rgb)
 
     # functions dealing with the colors of the rings of the laumio 
     def bottom_ring(color):
