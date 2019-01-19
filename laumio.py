1b75a3f256aa70738a9c388082acf5122021a236
Jnsll <june.benvegnu-sallou@irisa.fr>
2019-01-19 15:12:36 +0100

    update laumio

--- laumio.py
+++ laumio.py
@@ -18,18 +18,26 @@ class Laumio:
     # function changing the color of the whole laumio
     def all_blue(self):
         """ Change the color of all the LEDs of the laumio to blue"""
-        blue = utils.get_color_from_rgb('blue')
+        blue = utils.rgb_from_colorname('blue')
         topic = COMMAND_TARGET_TOPIC.format(name=self.name, cmd="fill")
         self.client.publish(topic, payload=blue)
 
-    def all_color():
+    def all_color([r, g, b]):
+
         ...
 
+    def all_color(color):
+
+        ...
 
     # functions dealing with the colors of the rings of the laumio 
-    def bottom_ring():
+    def bottom_ring(color):
         """ """
-        ...
+        topic = COMMAND_TARGET_TOPIC.format(name=self.name, cmd="set_ring")
+        color = utils.get_color_from_rgb(color)
+        message = 
+        self.client.publish(topic, payload=message)
+        
 
     def middle_ring():
         """ """
