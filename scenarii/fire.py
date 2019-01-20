2e85cca1b8809824f8c38c6c005a8534fb889932
Jnsll <june.benvegnu-sallou@irisa.fr>
2019-01-20 01:15:13 +0100

    try of scenario fire

--- scenarii/fire.py
+++ scenarii/fire.py
@@ -11,6 +11,7 @@ import time
 import random
 import laumio
 from laumio import Laumio
+import conf
 
 ## 21 anneaux en vertical
 ## 4 darkorange
@@ -21,7 +22,34 @@ from laumio import Laumio
 
 def main(laumios):
     laumios = tuple(laumios)
+    colors= ['yellow', 'gold', 'orange', 'darkorange']
+    #name = conf.SPATIAL_POSITION[9]
     
+    
+    for laumio in laumios:
+        if laumio.name == conf.SPATIAL_POSITION[9] or laumio.name == conf.SPATIAL_POSITION[8]:
+            laumio.fill('yellow')
+        elif laumio.name == conf.SPATIAL_POSITION[7]:
+            laumio.bottom_ring('yellow')
+            laumio.middle_ring('gold')
+            laumio.top_ring('gold')
+        elif laumio.name == conf.SPATIAL_POSITION[6] or laumio.name == conf.SPATIAL_POSITION[5]:
+            laumio.fill('gold')
+        elif laumio.name == conf.SPATIAL_POSITION[3] or laumio.name == conf.SPATIAL_POSITION[4]:
+            laumio.fill('orange')
+        elif laumio.name == conf.SPATIAL_POSITION[2]:
+            laumio.bottom_ring('orange')
+            laumio.middle_ring('orange')
+            laumio.top_ring('darkorange')
+        else :
+            laumio.fill('darkorange')
+    time.sleep(3)
+    for laumio in laumios:
+        laumio.off()
+    time.sleep(2)
+
+
+
     #Laumio 9 & 8 : color yellow
 
 
